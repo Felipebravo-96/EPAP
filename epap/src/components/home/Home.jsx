e90da56cab0e149f7779394c950b90d8e0da3077
DESKTOP-9VHM372\Felipe <felipebravo-96@hotmail.com>
2023-06-10 18:49:03 -0500

    First commit and files structure

---
+++ epap/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./home.module.css"
+import { Link } from "react-router-dom";
+
+function Home(){
+    return(
+        <div>
+            EPAP Humedal el charquito
+        </div>
+    )
+}
+
+export default Home
